603c4afecc59c6ab0d5b2f88082ad9c19d920cf1
bestplat <bestplat.com@gmail.com>
2013-09-11 11:09:36 +0800

    重构文件目录结构

--- bestplat-parent/bestplat-vssq/src/test/java/com/vssq/testcase/core/startup/StartupTest.java
+++ bestplat-parent/bestplat-vssq/src/test/java/com/vssq/testcase/core/startup/StartupTest.java
@@ -5,7 +5,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
-@ActiveProfiles("development")
+@ActiveProfiles("test")
 @ContextConfiguration(locations = { "classpath*:spring/*.xml" })
 public class StartupTest extends AbstractJUnit4SpringContextTests {
 
